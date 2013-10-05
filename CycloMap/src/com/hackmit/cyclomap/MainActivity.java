2480f1a4ac725d526e63db4c7aff60f2df81064d
Hayk Saribekyan <hayks@mit.edu>
2013-10-05 14:58:28 -0400

    Adding simple google map - doesn't work now.

--- CycloMap/src/com/hackmit/cyclomap/MainActivity.java
+++ CycloMap/src/com/hackmit/cyclomap/MainActivity.java
@@ -3,11 +3,10 @@ package com.hackmit.cyclomap;
 import android.app.Activity;
 import android.os.Bundle;
 
-
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.main);
     }
 }
