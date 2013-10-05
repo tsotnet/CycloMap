86d8fa94e3a227c26811947aa8e1def9871ac5ce
Tsotne Tabidze <tsotnet@mit.edu>
2013-10-05 16:28:40 -0400

    Create GoogleMap variable, turn on locations

--- CycloMap/src/com/hackmit/cyclomap/MainActivity.java
+++ CycloMap/src/com/hackmit/cyclomap/MainActivity.java
@@ -2,11 +2,30 @@ package com.hackmit.cyclomap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends Activity {
+
+    GoogleMap mMap;
+    MapFragment mMapFragment;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mMapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mMap = mMapFragment.getMap();
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(new OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                Log.d("TAG", ""+point.latitude+" "+point.longitude);
+            }
+        });
     }
 }
